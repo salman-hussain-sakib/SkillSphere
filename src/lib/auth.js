c5ea60662dc5fca764710ea3431d4749f1fd7fcb
salman-hussain-sakib <sakibsalmanh@gmail.com>
2026-05-01 01:14:35 +0600

    updgrade

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,10 +1,12 @@
 import { betterAuth } from "better-auth";
+import { mongoAdapter } from "@better-auth/mongo-adapter";
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URL || "");
+const db = client.db("skillsphere");
 
 export const auth = betterAuth({
-    database: {
-        provider: "mongodb",
-        url: process.env.MONGODB_URL
-    },
+    database: mongoAdapter(db),
     emailAndPassword: {
         enabled: true,
         autoSignIn: true
